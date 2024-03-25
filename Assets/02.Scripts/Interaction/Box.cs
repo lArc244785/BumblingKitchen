dcb60a42879b78630315c73d94230bdd77189309
lArc244785 <63329185+lArc244785@users.noreply.github.com>
2024-03-25 15:40:23 +0900

    feat : box
    pickableobject를 소환해줍니다.

--- Assets/02.Scripts/Interaction/Box.cs
+++ Assets/02.Scripts/Interaction/Box.cs
@@ -5,27 +5,9 @@ namespace BumblingKitchen.Interaction
 {
 	public class Box : NetworkBehaviour, IInteractable
 	{
+		[SerializeField] private Transform _spawnPoint;
 		[SerializeField] private NetworkPrefabRef _prefab;
 		public InteractionType Type => InteractionType.Box;
-
-		public bool CanInteraction(InteractionType type)
-		{
-			return type == InteractionType.Interactor;
-		}
-
-		public bool CanInteraction(Interactor interactor)
-		{
-			InteractionType type = interactor.HasPickUpObject ? interactor.GetPickUpObjectType() : InteractionType.Interactor;
-			return CanInteraction(type);
-		}
-
-		public void Interaction(Interactor interactor, IInteractable interactionObject)
-		{
-			if(CanInteraction(interactor) == true)
-			{
-				RPC_PickUpObject(interactor.GetComponent<NetworkObject>());
-			}
-		}
 
 		[Rpc(RpcSources.All, RpcTargets.StateAuthority)]
 		public void RPC_PickUpObject(NetworkId interactorID)
@@ -33,5 +15,24 @@ namespace BumblingKitchen.Interaction
 			var instance = Runner.Spawn(_prefab);
 			Runner.FindObject(interactorID).GetComponent<Interactor>().RPC_PickUp(instance);
 		}
+
+		public bool TryInteraction(Interactor interactor, IInteractable interactable)
+		{
+			if (interactor.HasPickUpObject == true)
+				return false;
+
+			RPC_SpawnPickUpObject(interactor.Object);
+			return true;
+		}
+
+		[Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+		public void RPC_SpawnPickUpObject(NetworkId pickUpUserID)
+		{
+			var interactor = Runner.FindObject(pickUpUserID).GetComponent<Interactor>();
+			var pickUpObject = Runner.Spawn(_prefab, _spawnPoint.position, _spawnPoint.rotation);
+			pickUpObject.transform.position = _spawnPoint.position;
+			interactor.RPC_PickUp(pickUpObject);
+		}
+
 	}
 }
