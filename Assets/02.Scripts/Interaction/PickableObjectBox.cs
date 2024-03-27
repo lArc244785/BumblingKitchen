081eb3658b6a5b56ee7547df47cd116034c8e69e
lArc244785 <63329185+lArc244785@users.noreply.github.com>
2024-03-28 00:35:55 +0900

    feat : pickableObjectBox

---
+++ Assets/02.Scripts/Interaction/PickableObjectBox.cs
@@ -0,0 +1,34 @@
+﻿using Fusion;
+using UnityEngine;
+
+namespace BumblingKitchen.Interaction
+{
+	public class PickableObjectBox : NetworkBehaviour, IInteractable
+	{
+		[SerializeField] private Transform _spawnPoint;
+		[SerializeField] private NetworkPrefabRef _prefab;
+
+		public InteractionType Type => InteractionType.Box;
+
+		public bool TryInteraction(Interactor interactor, IInteractable interactable)
+		{
+			if (interactor.HasPickUpObject == true)
+				return false;
+
+			RPC_Spawn(interactor.Object);
+			return true;
+		}
+
+		[Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+		public void RPC_Spawn(NetworkId pickUpUserID)
+		{
+			var interactor = Runner.FindObject(pickUpUserID).GetComponent<Interactor>();
+			var netObject = Runner.Spawn(
+				_prefab, 
+				interactor.PickUpPoint.position,
+				interactor.PickUpPoint.rotation);
+			
+			interactor.RPC_PickUp(netObject);
+		}
+	}
+}
