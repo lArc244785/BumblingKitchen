1570b9090e1d21d2e562b6df215133f4d89b8672
lArc244785 <63329185+lArc244785@users.noreply.github.com>
2024-04-03 01:05:05 +0900

    feat: exit app

--- Assets/02.Scripts/Title/ConnectToLobby.cs
+++ Assets/02.Scripts/Title/ConnectToLobby.cs
@@ -10,6 +10,7 @@ namespace BumblingKitchen.Title
 	public class ConnectToLobby : MonoBehaviour
 	{
 		[SerializeField] private Button _connetButton;
+		[SerializeField] private Button _exitButton;
 		[SerializeField] private FusionConnection _connectorPrefab;
 
 		private void Awake()
@@ -20,11 +21,21 @@ namespace BumblingKitchen.Title
 			}
 
 			_connetButton.onClick.AddListener(OnConnetingLobby);
+			_exitButton.onClick.AddListener(ExitGame);
 		}
 
 		private void OnConnetingLobby()
 		{
 			FusionConnection.Instance.ConnectToLobby();
+		}
+
+		private void ExitGame()
+		{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit();
+#endif
 		}
 	}
 }
